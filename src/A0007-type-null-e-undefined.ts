f98effe2a76c25f9dad4337b1600384d5c402f32
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-12-14 07:51:27 -0300

    add type undefined e null

---
+++ src/A0007-type-null-e-undefined.ts
@@ -0,0 +1,31 @@
+let x;
+
+if (typeof x === 'undefined') x = 20;
+
+console.log(x);
+
+export function createPerson(
+  firstName: string,
+  lastName?: string,
+): {
+  firstName: string;
+  lastName?: string;
+} {
+  return {
+    firstName,
+    lastName,
+  };
+}
+
+export function squareOf(x: any) {
+  if (typeof x === 'number') return x * x;
+  return null;
+}
+
+const squareOfTwoNumber = squareOf(2);
+
+if (squareOfTwoNumber === null) {
+  console.log('conta inválida');
+} else {
+  console.log(squareOfTwoNumber * 100);
+}
