0898f4d7e5900eeb5090ad508b3cffc2d591983f
AndersonCR <anderson.c.rodrigues@hotmail.com>
2022-12-14 08:49:57 -0300

    add type union

---
+++ src/A0011-type-union.ts
@@ -0,0 +1,11 @@
+// string | number | boolean = n]ao é union type
+
+function add(a: number | string, b: number | string): number | string {
+  if (typeof a === 'number' && typeof b === 'number') return a + b;
+  return `${a}${b}`;
+}
+
+console.log(add(10, 20));
+console.log(add('10', '20'));
+console.log(add('10', 20));
+console.log(add(10, '20'));
